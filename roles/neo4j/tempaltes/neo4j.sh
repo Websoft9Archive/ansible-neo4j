8daf353eed8802ff49ce9988c87bb86fdac5fa61
qiaofeng1227 <76487013@qq.com>
2020-08-12 16:04:54 +0800

    Delete neo4j.sh

--- roles/neo4j/tempaltes/neo4j.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo " 
-neo4j
-neo4j 
-neo4j123" | cypher-shell
-
-echo "
-ALTER CURRENT USER SET PASSWORD FROM 'neo4j123' TO 'neo4j';
-" | cypher-shell -u neo4j  -p neo4j123  -d system
