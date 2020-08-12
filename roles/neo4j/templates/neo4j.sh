b37441092a11e91637958b3ea37cd42207954ec3
qiaofeng1227 <76487013@qq.com>
2020-08-12 19:03:40 +0800

    Update neo4j.sh

--- roles/neo4j/templates/neo4j.sh
+++ roles/neo4j/templates/neo4j.sh
@@ -3,8 +3,8 @@ sleep 15s
 
 echo "
 ALTER CURRENT USER SET PASSWORD FROM 'neo4j' TO 'neo4j123';
-" | cypher-shell -u neo4j  -p neo4j
+" | cypher-shell -u neo4j  -p neo4j  -d system
 
 echo "
 ALTER CURRENT USER SET PASSWORD FROM 'neo4j123' TO 'neo4j';
-" | cypher-shell -u neo4j  -p neo4j123
+" | cypher-shell -u neo4j  -p neo4j123  -d system
